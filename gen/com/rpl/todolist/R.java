91a831d91c383b234f7fd75daaa52a4539f5c457
Raphael <raphael.lisboa@gmail.com>
2013-09-08 14:20:08 -0300

    Nova telade cadastro

--- gen/com/rpl/todolist/R.java
+++ gen/com/rpl/todolist/R.java
@@ -21,26 +21,48 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_formalario=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int botao=0x7f080002;
-        public static final int input=0x7f080001;
-        public static final int listagem=0x7f080000;
+        public static final int action_settings=0x7f08000c;
+        public static final int botao=0x7f08000b;
+        public static final int data=0x7f080005;
+        public static final int descricao=0x7f080003;
+        public static final int input=0x7f08000a;
+        public static final int listagem=0x7f080009;
+        public static final int local=0x7f080007;
+        public static final int salvar=0x7f080008;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
+        public static final int textView3=0x7f080004;
+        public static final int textView4=0x7f080006;
+        public static final int titulo=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_list=0x7f030000;
+        public static final int activity_formulario=0x7f030000;
+        public static final int activity_list=0x7f030001;
     }
     public static final class menu {
-        public static final int list=0x7f070000;
+        public static final int formulario=0x7f070000;
+        public static final int list=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int cadastro_de_nova_atividade=0x7f05000c;
+        public static final int data=0x7f05000a;
+        public static final int descri_o_da_atividade=0x7f050009;
+        public static final int descri_o_da_atividade1=0x7f050007;
         public static final int hello_world=0x7f050002;
+        public static final int local=0x7f05000b;
         public static final int nova_tarefa=0x7f050003;
+        public static final int rua_de_la_n100=0x7f05000e;
+        public static final int rua_de_la_n_100=0x7f05000d;
         public static final int salvar=0x7f050004;
+        public static final int title_activity_formulario=0x7f050005;
+        public static final int titulo=0x7f050008;
+        public static final int titulo_da_atividade=0x7f050006;
     }
     public static final class style {
         /** 
